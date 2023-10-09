ca320cb9a446c3afb984593efe3debb3b2c96777
loganjsch <loganjsch@gmail.com>
2023-10-09 13:13:29 -0700

    Update carts.py

--- src/api/carts.py
+++ src/api/carts.py
@@ -50,11 +50,6 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
-    """
-    total_potions_bought = 0
-    for item_val in carts[cart_id - 1].values():
-        total_potions_bought += int(item_val)
-    """
     cart_index = cart_id - 1
 
     if cart_index < 0 or cart_index >= len(carts):
@@ -65,9 +60,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     total_gold_paid = 0
 
     for item_sku, quantity in cart.items():
-        # You may need to define a mapping from item_sku to item price
-        # For example, if item_sku is the SKU of the item, you can fetch its price from a database or a predefined dictionary.
-        # Assuming item_price is a placeholder for the item's price:
         item_price = get_item_price(item_sku)
 
         if item_price is not None:
@@ -75,26 +67,17 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             total_gold_paid += item_price * quantity
 
     # Now you have the total_potions_bought and total_gold_paid for the cart
-    # You can perform further actions such as updating the database here.
 
-    
-
-    return {"total_potions_bought": total_potions_bought, "total_gold_paid": total_gold_paid}
-
-    total_gold_paid = 50 * total_potions_bought
-    # sql goes here 
-
-    '''
-    
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = num_red_potions - " + str(total_potions_bought)))
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold = gold + " + str(total_gold_paid)))
 
-    '''
+    return {"total_potions_bought": total_potions_bought, "total_gold_paid": total_gold_paid}
+
 
 def get_item_price(item_sku):
     # Replace this with your logic to fetch the item's price from a database or a dictionary
     # Example:
-    item_prices = {"RED_POTION_0": 50, "item_sku_2": 15, "item_sku_3": 20}
+    item_prices = {"RED_POTION_0": 50, "GREEN_POTION_0": 15, "item_sku_3": 20}
     return item_prices.get(item_sku, None)
